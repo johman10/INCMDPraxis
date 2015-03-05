7b2e060ae4d4d2c5a488fffaeed327558ce8b31d
Johman10 <johveck@gmail.com>
2015-03-05 19:27:25 +0100

    Code formatting + cleanup

--- index.js
+++ index.js
@@ -6,34 +6,34 @@ app.set('port', (process.env.PORT || 5000));
 var mongoose = require('mongoose');
 
 mongoose.connect('Johman10:1234567890@ds039251.mongolab.com:39251/mower_preorders', function (error) {
-    if (error) {
-        console.log(error);
-    }
+  if (error) {
+    console.log(error);
+  }
 });
 
 var userSchema = mongoose.Schema({
-        name: String
-      });
+  name: String
+});
 
 var User = mongoose.model('users', userSchema);
 
 app.get('/getuser', function (req, res, next) {
-    User.find({}, function (err, docs) {
-       res.json(docs);
-    });
+  User.find({}, function (err, docs) {
+   res.json(docs);
+ });
 });
 
 app.get('/createuser', function(req, resp) {
-    var newUser = new User({name: req.query.userName});
+  var newUser = new User({name: req.query.userName});
     newUser.save(function(err){ // will this callback always be called correctly?
-        if(err) {
-            resp.send('ERROR!');
-        }
-        else {
-            resp.redirect('/');
-        }
+      if(err) {
+        resp.send('ERROR!');
+      }
+      else {
+        resp.redirect('/');
+      }
     });
-});
+  });
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
